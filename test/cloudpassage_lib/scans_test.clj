5bc706ae095f3db196acc6eb47ed28a040d8e7d8
Tim Simpson <tim.simpson@rackspace.com>
2016-03-17 09:01:57 -0500

    Change API calls used to get scan data
    Previously we've been using the scans API to fetch data by time to
    generate our report. This PR changes it to fetch the most recent
    scans for each server (aka host) which keeps us from having to
    use large time periods in our query to make sure we get some data
    for each server while also having to throw our duplicate info for
    any server which may appear.

--- test/cloudpassage_lib/scans_test.clj
+++ test/cloudpassage_lib/scans_test.clj
@@ -44,6 +44,10 @@
   (is (= "https://abc.com/v1/scans/abcdef/findings/xyzzy"
          (#'scans/finding-detail-url "https://abc.com/" "abcdef" "xyzzy"))))
 
+(deftest scan-server-url-tests
+  (is (= "https://api.cloudpassage.com/v1/servers/server-id/svm"
+         (#'scans/scan-server-url "server-id" "svm"))))
+
 (defn ^:private index->module
   "Given an index of a (fake, test-only) scan, return a module for that scan."
   [i]
@@ -112,16 +116,32 @@
    cskc/->snake_case_keyword
    (fake-get-page! client-id client-secret url)))
 
+(defn ^:private fake-get-page-with-bad-response!
+  "Like fake-get-page, but returns a bad status code."
+  [client-id client-secret url]
+  (is (= client-id "lvh"))
+  (is (= client-secret "hunter2"))
+  :cloudpassage-lib.core/fetch-error)
+
 (deftest scans!-tests
-  (with-redefs [scans/get-page! fake-get-page!]
-    (let [scans-stream (scans/scans! "lvh" "hunter2" {"modules" "fim"})
-          scans (ms/stream->seq scans-stream)]
-      (is (= (for [scan-id (range (* fake-pages scans-per-page))]
-               {:scan-id scan-id
-                :module (index->module scan-id)
-                :url details-query-url})
-             scans))
-      (is (ms/closed? scans-stream)))))
+  (testing "Successful scan with pagination."
+    (with-redefs [scans/get-page! fake-get-page!]
+      (let [scans-stream (scans/scans! "lvh" "hunter2" {"modules" "fim"})
+            scans (ms/stream->seq scans-stream)]
+        (is (= (for [scan-id (range (* fake-pages scans-per-page))]
+                 {:scan-id scan-id
+                  :module (index->module scan-id)
+                  :url details-query-url})
+               scans))
+        (is (ms/closed? scans-stream)))))
+  (testing "Error occurs fetching page."
+    ;;TODO: Replacing the error thing to detect it was logged might be helpful.
+    (with-redefs [scans/get-page! fake-get-page-with-bad-response!]
+      (let [scans-stream (scans/scans! "lvh" "hunter2" {"modules" "fim"})
+            result (clojure.string/join "" (ms/stream->seq scans-stream))]
+        (is (ms/drained? scans-stream))
+        (is (ms/closed? scans-stream))
+        (is (= result ""))))))
 
 (deftest scans-with-details!-tests
   (testing "Typical scan returns expected page details."
@@ -147,28 +167,112 @@
            ms/stream->seq
            doall))))
 
+(defn ^:private make-fake-page
+  "Generates a fake page function. Give it the expected base path and a
+   function to call with the current page number."
+  [cb]
+  (fn [client-id client-secret url]
+    (is (= client-id "lvh"))
+    (is (= client-secret "hunter2"))
+    (let [parsed-url (u/url url)
+          path (:path parsed-url)
+          query (:query parsed-url)
+          page-num (-> query
+                       (get "page" "0")
+                       Integer/parseInt)]
+      (cb {:page-num page-num :path path}))))
+
+(defn fake-servers-page
+  "Pretends to be the servers list page results."
+  [page-two-breaks]
+  (make-fake-page (fn [{:keys [page-num path]}]
+                    (is (= "/v1/servers" path))
+                    (case page-num
+                      0 {:servers [{:id "server-id-1"} {:id "server-id-2"}]
+                         :pagination {:next (str @#'scans/base-servers-url "?page=1")}}
+                      1 (if page-two-breaks
+                          :cloudpassage-lib.core/fetch-error
+                          {:servers [{:id "server-id-3"}]})))))
+
+(deftest list-servers!-tests
+  (testing "Make sure the server call returns some servers."
+    (with-redefs [scans/get-page! (fake-servers-page false)]
+      (let [server-stream (scans/list-servers! "lvh" "hunter2")
+            id-list (map :id (ms/stream->seq server-stream))]
+        (is (= ["server-id-1" "server-id-2" "server-id-3"] id-list))
+        (is (ms/closed? server-stream)))))
+  (testing "Make sure we catch a bad response on page 2."
+    (with-redefs [scans/get-page! (fake-servers-page true)]
+      (let [server-stream (scans/list-servers! "lvh" "hunter2")
+            id-list (map :id (ms/stream->seq server-stream))]
+        (is (= ["server-id-1" "server-id-2"] id-list))
+        (is (ms/closed? server-stream))))))
+
+(deftest scan-server-tests
+  (with-redefs [scans/get-page! (make-fake-page
+                                 (fn [{:keys [path]}]
+                                   (is (= "/v1/servers/server-id-here/svm" path))
+                                   "GOOD"))]
+    (is (= "GOOD" (scans/scan-server "lvh" "hunter2" "server-id-here" "svm")))))
+
+(deftest scan-each-server!-tests
+  (testing "Scan can handle an empty stream."
+    (let [input (ms/stream)]
+      (ms/close! input)
+      (let [scan-stream (scans/scan-each-server! "lvh" "hunter2" "svm"
+                                                 input)
+            scan-result (clojure.string/join "" (ms/stream->seq scan-stream))]
+        (is (= "" scan-result)))))
+  (testing "When the server scan page is broken, we get back nadda."
+    (with-redefs [scans/get-page! (make-fake-page
+                                   (fn [{:keys [path]}]
+                                     (case path
+                                       "/v1/servers/server-id-here/svm"
+                                       :cloudpassage-lib.core/fetch-error)))]
+      (let [input (ms/stream)]
+        (ms/put! input {:id "server-id-here"})
+        (ms/close! input)
+        (let [scan-stream (scans/scan-each-server! "lvh" "hunter2" "svm"
+                                                   input)
+              scan-result (clojure.string/join "" (ms/stream->seq scan-stream))]
+          (is (= "" scan-result))))))
+  (testing "Server scan can get back several pages of data."
+    (with-redefs [scans/get-page! (make-fake-page
+                                   (fn [{:keys [path]}]
+        ;; The real data returning from a valid scan-server call would be a
+        ;; map containing a bunch of scan data. But for testing purposes all
+        ;; that matters is that scan-each-server! puts whatever comes back
+        ;; into the its output stream.
+                                     (case path
+                                       "/v1/servers/server-1/svm" "ONE"
+                                       "/v1/servers/server-2/svm" "TWO"
+                                       "/v1/servers/server-3/svm" "THREE!")))]
+      (let [input (ms/stream)]
+        (ms/put! input {:id "server-1"})
+        (ms/put! input {:id "server-2"})
+        (ms/put! input {:id "server-3"})
+        (ms/close! input)
+        (let [scan-stream (scans/scan-each-server! "lvh" "hunter2" "svm"
+                                                   input)
+              scan-result (clojure.string/join "..." (ms/stream->seq scan-stream))]
+          (is (= "ONE...TWO...THREE!" scan-result)))))))
+
 (defn ^:private test-report
   [report-fn! expected-module]
-  (with-redefs [scans/get-page! fake-get-page!]
-    (let [report (report-fn! "lvh" "hunter2")]
-      (is (= (for [scan-id (range (* fake-pages scans-per-page))
-                   :let [module (index->module scan-id)]
-                   :when (= module expected-module)]
-               {:scan-id scan-id
-                :module module
-                :url details-query-url
-                :scan {}})
-             report))))
-  (with-redefs [scans/get-page! fake-get-page-with-snakes]
-    (let [report (report-fn! "lvh" "hunter2")]
-      (is (= (for [scan-id (range (* fake-pages scans-per-page))
-                   :let [module (index->module scan-id)]
-                   :when (= module expected-module)]
-               {:scan-id scan-id
-                :module module
-                :url details-query-url
-                :scan {}})
-             report)))))
+  (with-redefs [scans/get-page! (make-fake-page
+                                 (fn [{:keys [path]}]
+                                   (case path
+                                     "/v1/servers" {:servers [{:id "server-id-1"} {:id "server-id-2"}]}
+                                     "/v1/servers/server-id-1/fim" {:id "1" :scan {}}
+                                     "/v1/servers/server-id-2/fim" {:id "2" :scan {}}
+                                     "/v1/servers/server-id-1/svm" {:id "1" :scan {}}
+                                     "/v1/servers/server-id-2/svm" {:id "2" :scan {}}
+                                     "/v1/servers/server-id-1/sca" {:id "1" :scan {}}
+                                     "/v1/servers/server-id-2/sca" {:id "2" :scan {}})))]
+    (let [report (report-fn! "lvh" "hunter2")
+          result (clojure.string/join "..." report)]
+      (is (= (str {:id "1", :scan {}} "..." {:id "2", :scan {}})
+             result)))))
 
 (deftest fim-report!-tests
   (test-report scans/fim-report! "fim"))
